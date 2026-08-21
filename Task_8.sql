3ce026bf4eeebd8b0fddf2e6d2f05cea44af6674
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-08-21 23:11:50 +0530

    Add files via upload

---
+++ Task_8.sql
@@ -0,0 +1,17 @@
+-- SQL Practice
+-- Q8.Display all employees whose Salary is between 30000 and 45000.
+
+CREATE DATABASE sql_practice_db;
+USE sql_practice_db;
+
+CREATE TABLE EMPLOYEE (Emp_id INT PRIMARY KEY , Name VARCHAR(50),Department VARCHAR(50),Age INT,Salary DECIMAL);
+INSERT EMPLOYEE VALUES 
+( 1 , "Rohan","Robotics",21,25000),
+( 2 , "Sneha","IT",24,35000),
+( 3 , "Amit","HR",28,30000), 
+( 4 , "Priya","IT",22,40000),
+( 5 , "Rahul","Robotics",30,45000); 
+
+-- Solution
+
+SELECT* from Employee where Salary between 30000 and 45000;
