024404d5d8c84e18dad32b5cbceeac9ab5c5963d
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-08-21 22:17:09 +0530

    Add files via upload

---
+++ Task_1.sql
@@ -0,0 +1,17 @@
+-- SQL Practice
+-- Q1: Display all employees
+
+CREATE DATABASE sql_practice_db;
+USE sql_practice_db;
+
+CREATE TABLE EMPLOYEE (Emp_id INT PRIMARY KEY , Name VARCHAR(50),Department VARCHAR(50),Age INT,Salary DECIMAL);
+INSERT EMPLOYEE VALUES 
+( 1 , "Rohan","Robotics",21,25000),
+( 2 , "Sneha","IT",24,35000),
+( 3 , "Amit","HR",28,30000), 
+( 4 , "Priya","IT",22,40000),
+( 5 , "Rahul","Robotics",30,45000); 
+
+-- Solution
+
+SELECT * FROM EMPLOYEE ;
